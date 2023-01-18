1d92a22a7ff25b9157e2341a238d7518552e4ec4
DaniSchober <d.schober@tum.de>
2023-01-18 18:51:15 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dvc pull
 wandb login $WANDB_API_KEY
-python -u src/data/make_dataset.py preprocess-command --num_samples=100
+python -u src/data/make_dataset.py preprocess-command --num_samples=20
 python -u src/models/train_model.py $EXP
